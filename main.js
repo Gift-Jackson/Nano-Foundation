1c46ffc69d3615ded00e5ef22a0acf5ba25391b9
Gift-Jackson <113178450+Gift-Jackson@users.noreply.github.com>
2023-09-14 09:39:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,66 +2,66 @@ const header = document.querySelector("header");
 const toggle = document.querySelector(".toggle-box");
 const toggleBar = document.querySelector(".toggle");
 const ul = document.querySelector("ul");
-const btn = document.querySelector(".btn");
+const btn = document.querySelector(".hero-btn");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 const link = document.querySelectorAll(".link");
 const input = document.querySelector("#address");
-const copyBtn = document.querySelector(".copy");
+const copyBtn = document.querySelector(".copy-btn");
 
 let lastScroll = 0;
 
-window.addEventListener("scroll", ()=>{
-    if(scrollY > lastScroll){
+window.addEventListener("scroll", () => {
+    if (scrollY > lastScroll) {
         header.style.top = "-100px";
     }
-    else{
+    else {
         header.style.top = "0";
     }
 
     lastScroll = window.scrollY;
 });
 
-toggle.addEventListener("click", ()=>{
+toggle.addEventListener("click", () => {
     toggleBar.classList.toggle("active");
 
-    if(toggleBar.classList.contains("active")){
+    if (toggleBar.classList.contains("active")) {
         ul.classList.add("active");
     }
-    else{
+    else {
         ul.classList.remove("active");
     }
 });
 
-btn.addEventListener("click", ()=>{
+btn.addEventListener("click", () => {
     overlay.style.display = "flex";
 });
-close.addEventListener("click", ()=>{
+close.addEventListener("click", () => {
     overlay.style.display = "none";
 });
-overlay.addEventListener("click", (e)=>{
+overlay.addEventListener("click", (e) => {
     overlay.style.display = "none";
 
-    if(e.target !== overlay && e.target !== close){
-        overlay.style.display = "grid";
+    if (e.target !== overlay && e.target !== close) {
+        overlay.style.display = "flex";
     }
 });
 
-link.forEach((e)=>{
-    e.addEventListener("click", ()=>{
+link.forEach((e) => {
+    e.addEventListener("click", () => {
         ul.classList.remove("active");
         toggleBar.classList.remove("active");
-    })
+    });
 });
 
-copyBtn.addEventListener("click", ()=>{
+copyBtn.addEventListener("click", () => {
     copyBtn.textContent = "Copied!";
 
     input.select();
 
     document.execCommand("copy");
 
-    setTimeout(function(){
+    setTimeout(function () {
         copyBtn.textContent = "Copy";
-    },2500);
+    }, 2500);
 })
